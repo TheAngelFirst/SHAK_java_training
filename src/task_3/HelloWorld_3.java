ed08faed45cd90057e0f25813128f0c7e68e0885
TheAngelFirst <angel-first@bk.ru>
2020-01-23 10:15:37 +0300

    Выполнено 3 задание

---
+++ src/task_3/HelloWorld_3.java
@@ -0,0 +1,13 @@
+package task_3;
+
+/*
+ Пример простой программы на Java
+ Ширяев А.К.
+ 23.01.2020
+*/
+
+public class HelloWorld_3 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
