fd82dbf4b0c92dbaca834fdf16230b479ea13703
TheAngelFirst <angel-first@bk.ru>
2020-01-23 16:10:42 +0300

    Выполнено 14 задание

--- src/Test.java
+++ src/Test.java
@@ -4,12 +4,13 @@ import java.io.InputStreamReader;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        double[] i = {11.0, 5.8, 11.1, 10.9};
-        double min  = Double.MAX_VALUE;
-        for (double x:i) {
-            min = Math.min(min, x);
-        }
-        System.out.println(min);
+        float num = 5.25f;
+
+        System.out.println(Math.round(num));
+
+        System.out.println(Math.floor(num));
+
+        System.out.println(Math.ceil(num));
 
 
 
