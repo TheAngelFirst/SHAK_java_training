ccf288a870a1d888f55002810a08242e5dfc0536
TheAngelFirst <angel-first@bk.ru>
2020-01-24 09:07:22 +0300

    Исправлено 6 задание

--- src/task_6/ArithmeticOperations_6.java
+++ src/task_6/ArithmeticOperations_6.java
@@ -1,5 +1,11 @@
 package task_6;
-
+/*
+Написать программу, которая будет выполнять следующие действия:
+1. Ввод трех чисел с клавиатуры x, y, z
+2. Нахождение и вывод на экран среднего арифметического чисел x, y, z
+3. Деление среднего арифметического на 2 без остатка
+4. Если полученное число больше 3, то вывести на экран сообщение "Программа выполнена корректно"
+ */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,8 +17,8 @@ public class ArithmeticOperations_6 {
         int x = Integer.parseInt(reader.readLine()),
                 y = Integer.parseInt(reader.readLine()),
                 z = Integer.parseInt(reader.readLine());
-        double av = (x + z + y) / 3;
+        double av = (double) (x + z + y) / 3;
         System.out.printf("Среднее арифмитическое - %.2f%n", av);
-        if (av / 2 > 3) System.out.print("Программа выполнена корректно");
+        if ((int) av / 2 > 3) System.out.print("Программа выполнена корректно");
     }
 }
